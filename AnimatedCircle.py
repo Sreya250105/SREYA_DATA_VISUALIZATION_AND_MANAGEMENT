2dfc93106af7dd90a01bfe8cecfabc56a4a41026
Sreya250105 <sreyanandi25@gmail.com>
2026-02-23 11:53:06 +0530

    Display animated circle with plt.show()
    Added plt.show() to display the animation.

--- AnimatedCircle.py
+++ AnimatedCircle.py
@@ -9,11 +9,9 @@ ax.set_ylim(0, 10)
 ax.set_aspect('equal')
 ax.set_title("Animated Circle")
 
-# Create circle
 circle = plt.Circle((0, 5), 0.2, color='blue')
 ax.add_patch(circle)
 
-# Animation function
 def animate(i):
     circle.center = (i * 0.1, 5)   # Move circle horizontally
     return circle,
@@ -25,5 +23,6 @@ ani = animation.FuncAnimation(
     interval=50,
     blit=True
 )
+
 
 plt.show()
